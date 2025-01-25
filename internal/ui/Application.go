94f81a21b6f45301c1050594278c5bc0be6a4193
Latent <guillaume.thibault.98@proton.me>
2025-01-24 21:23:49 -0500

    refact(structure): cmd to swtich UI

--- internal/ui/Application.go
+++ internal/ui/Application.go
@@ -13,16 +13,16 @@ import (
 	"github.com/charmbracelet/log"
 )
 
-type state int
+type CurrentUI int
 
 const (
-	noteUI state = iota
-	createUI
-	loginUI
+	NoteUI CurrentUI = iota
+	CreateUI
+	LoginUI
 )
 
 type Model struct {
-	state  state
+	state  CurrentUI
 	notes  NotesUI.Model
 	create create.Model
 	login  login.Model
@@ -32,27 +32,42 @@ func (m Model) Init() tea.Cmd {
 	return nil
 }
 
-func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch m.state {
+type SwitchUIMsg struct {
+	NewState CurrentUI
+}
 
-	case loginUI:
+func SwitchUICmd(newState CurrentUI) tea.Cmd {
+	return func() tea.Msg {
+		return SwitchUIMsg{NewState: newState}
+	}
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+
+	// Global Signal to swtich UI
+	switch msg := msg.(type) {
+	case SwitchUIMsg:
+		m.state = msg.NewState
+		return m, nil
+	}
+
+	// Display Update
+	switch m.state {
+	case LoginUI:
 		var cmd tea.Cmd
 		loginModel, cmd := m.login.Update(msg)
 		m.login = loginModel.(login.Model)
 		return m, cmd
-
-	case noteUI:
+	case NoteUI:
 		var cmd tea.Cmd
 		notesModel, cmd := m.notes.Update(msg)
 		m.notes = notesModel.(NotesUI.Model)
 		return m, cmd
-
-	case createUI:
+	case CreateUI:
 		var cmd tea.Cmd
 		createModel, cmd := m.create.Update(msg)
 		m.create = createModel.(create.Model)
 		return m, cmd
-
 	default:
 		return m, nil
 	}
@@ -60,9 +75,9 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m Model) View() string {
 	switch m.state {
-	case loginUI:
+	case LoginUI:
 		return m.login.View()
-	case noteUI:
+	case NoteUI:
 		return m.notes.View()
 	default:
 		return m.create.View()
@@ -71,13 +86,13 @@ func (m Model) View() string {
 
 func NewModel(credentialsManager *auth.CredentialsManager, themeManager *styles.ThemeManager) (Model, error) {
 	// Verify user credentials
-	initialUI := loginUI
+	initialUI := LoginUI
 	var client *api.Client = nil
 	var err error = nil
 
 	creds, _ := credentialsManager.LoadCredentials()
 	if creds != nil {
-		initialUI = noteUI
+		initialUI = NoteUI
 		client, err = api.NewClient(creds)
 		if err != nil {
 			log.Error("Failed to create API client: %v", err)
@@ -100,8 +115,6 @@ func NewModel(credentialsManager *auth.CredentialsManager, themeManager *styles.
 	if err != nil {
 		return Model{}, fmt.Errorf("failed to create create model: %w", err)
 	}
-
-	// Determine which UI to start
 
 	return Model{
 		state:  initialUI,
