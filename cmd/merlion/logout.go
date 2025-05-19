ed54beaff059059e9a1ae3c6490defa90c5bb8f9
Latent <guillaume.thibault.98@proton.me>
2025-05-19 18:18:52 -0400

    refact(cmd): split cmd into multiple files

---
+++ cmd/merlion/logout.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func LogoutCmd(args ...string) int {
+	fmt.Println("Unimplemented")
+	return 1
+}
--- cmd/merlion/main.go
+++ cmd/merlion/main.go
@@ -3,19 +3,9 @@ package main
 import (
 	"fmt"
 	version "merlion/cmd/merlion/version"
-	"merlion/internal/store/cloud"
-	"merlion/internal/styles"
-	"merlion/internal/ui"
-	"merlion/internal/utils"
-	"net/http"
 	_ "net/http/pprof"
 	"os"
-	"path/filepath"
 	"strings"
-	"time"
-
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/log"
 )
 
 type Command struct {
@@ -50,77 +40,9 @@ func init() {
 		{
 			name:        "logout",
 			description: "Removed the cached credentials",
-			run:         func(args ...string) int { return 0 },
+			run:         LogoutCmd,
 		},
 	}
-}
-
-func UI() {
-	startTime := time.Now()
-	closer, err := utils.SetupLog()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	log.Debug("Starting main function", "time", startTime)
-	log.Debug("Log setup complete", "elapsed", time.Since(startTime))
-
-	if os.Getenv("APP_ENV") == "dev" {
-		log.Info("Enabling pprof for profiling")
-		go func() {
-			log.Info(http.ListenAndServe("localhost:6060", nil))
-		}()
-	}
-
-	// Initialize config directory
-	userConfigDir, err := os.UserConfigDir()
-	if err != nil {
-		_ = closer()
-		log.Fatalf("Failed to get user config directory: %v", err)
-	}
-	configDir := filepath.Join(userConfigDir, "merlion")
-	if err := os.MkdirAll(configDir, 0700); err != nil {
-		log.Fatalf("Failed to create config directory: %v", err)
-	}
-	log.Debug("Config directory initialized", "elapsed", time.Since(startTime))
-
-	// Initialize theme manager
-	themeManager, err := styles.NewThemeManager(configDir)
-	if err != nil {
-		_ = closer()
-		log.Fatalf("Failed to initialize theme manager: %v", err)
-	}
-	log.Debug("Theme manager initialized", "elapsed", time.Since(startTime))
-
-	// Initialize credentials manager
-	credMgr, err := cloud.NewCredentialsManager()
-	if err != nil {
-		_ = closer()
-		log.Fatalf("Failed to initialize credentials manager: %v", err)
-	}
-	log.Debug("Credentials manager initialized", "elapsed", time.Since(startTime))
-
-	model, err := ui.NewModel(credMgr, themeManager)
-	if err != nil {
-		_ = closer()
-		log.Fatalf("Failed to create UI model: %v", err)
-	}
-	log.Debug("UI model created", "elapsed", time.Since(startTime))
-
-	p := tea.NewProgram(
-		model,
-		tea.WithAltScreen(),
-		tea.WithMouseCellMotion(),
-	)
-	log.Debug("Tea program created", "elapsed", time.Since(startTime))
-
-	log.Debug("Starting Tea program", "elapsed", time.Since(startTime))
-	if _, err := p.Run(); err != nil {
-		_ = closer()
-		log.Fatalf("Error running program: %v", err)
-	}
-	_ = closer()
-	log.Debug("Main function completed", "total_time", time.Since(startTime))
 }
 
 func main() {
@@ -136,5 +58,5 @@ func main() {
 		HelpCmd()
 		os.Exit(1)
 	}
-	UI()
+	startTUI()
 }
