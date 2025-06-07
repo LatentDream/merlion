08d863064f7215fa20cf1ab0e94468f45149dd54
Latent <guillaume.thibault.98@proton.me>
2025-06-07 17:29:12 +0000

    fix(migrations): missing `,` in sql file 001

--- internal/store/local/database/migrations/001__create_notes.sql
+++ internal/store/local/database/migrations/001__create_notes.sql
@@ -1,7 +1,7 @@
 CREATE TABLE notes (
     note_id	 UUID PRIMARY KEY,
     title	 TEXT NOT NULL,
-    content	 TEXT
+    content	 TEXT,
     tags	 TEXT[],
     is_favorite	 BOOLEAN NOT NULL DEFAULT false,
     is_work_log  BOOLEAN NOT NULL DEFAULT false,
